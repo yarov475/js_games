4afb2d682899c63f91ce7a065be59f68f5c4f74d
yarov475 <ya.yarochkin@yandex.ru>
2021-09-29 21:18:33 +0100

    fix: align items

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', ()=>{
+document.addEventListener('DOMContentLoaded', () => {
     const cardArray = [
 
         {
@@ -52,53 +52,55 @@ document.addEventListener('DOMContentLoaded', ()=>{
 
 
     ]
-cardArray.sort(()=>0.5-Math.random())
+    cardArray.sort(() => 0.5 - Math.random())
     const grid = document.querySelector('.grid');
     const resultDisplay = document.querySelector('#result')
     var cardsChosen = []
     var cardsChosenID = []
     var cardsWon = []
 
-function  createBoard(){
-    for (let i = 0; i<cardArray.length; i++){
-        var card = document.createElement('img')
-        card.setAttribute('src', 'images/blank.png');
-        card.setAttribute('data-id', i)
-        card.addEventListener('click',flipCard)
-        grid.appendChild(card)
+    function createBoard() {
+        for (let i = 0; i < cardArray.length; i++) {
+            var card = document.createElement('img')
+            card.setAttribute('src', 'images/blank.png');
+            card.setAttribute('data-id', i)
+            card.addEventListener('click', flipCard)
+            grid.appendChild(card)
+        }
     }
-}
-   createBoard();
-    function checkForMatch(){
+
+    createBoard();
+
+    function checkForMatch() {
         var cards = document.querySelectorAll('img')
         const optionOneId = cardsChosenID[0]
         const optionTwoId = cardsChosenID[1]
-        if (cardsChosen[0]===cardsChosen[1]){
+        if (cardsChosen[0] === cardsChosen[1]) {
             alert('It is a match!')
             cards[optionOneId].setAttribute('src', 'images/white.png')
             cards[optionTwoId].setAttribute('src', 'images/white.png')
             cardsWon.push(cardsChosen)
-        }else {
-            cards[optionOneId].setAttribute('src','images/blank.png')
-            cards[optionTwoId].setAttribute('src','images/blank.png')
+        } else {
+            cards[optionOneId].setAttribute('src', 'images/blank.png')
+            cards[optionTwoId].setAttribute('src', 'images/blank.png')
             alert('one more time')
         }
-        cardsChosen=[]
-        cardsChosenID=[]
-        resultDisplay.textContent= cardsWon.length
-        if(cardsWon.length === cardArray.length/2){
+        cardsChosen = []
+        cardsChosenID = []
+        resultDisplay.textContent = cardsWon.length
+        if (cardsWon.length === cardArray.length / 2) {
 
             document.write("<h1>Winner</h1>");
         }
     }
 
-    function flipCard(){
-    var cardId = this.getAttribute('data-id')
-    cardsChosen.push(cardArray[cardId].name)
-    cardsChosenID.push(cardId)
-    this.setAttribute('src', cardArray[cardId].img)
-        if(cardsChosen.length===2){
-            setTimeout(checkForMatch,500)
+    function flipCard() {
+        var cardId = this.getAttribute('data-id')
+        cardsChosen.push(cardArray[cardId].name)
+        cardsChosenID.push(cardId)
+        this.setAttribute('src', cardArray[cardId].img)
+        if (cardsChosen.length === 2) {
+            setTimeout(checkForMatch, 500)
         }
 
     }
