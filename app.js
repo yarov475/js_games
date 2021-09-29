cbe75b332d975e500beac3104d58d60f1d664372
yarov475 <ya.yarochkin@yandex.ru>
2021-09-29 21:01:43 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,80 @@
+document.addEventListener('DOMContentLoaded', ()=>{
+    const cardArray = [
+
+        {
+            name: 'elephant',
+            img: 'images/elephant.png'
+        },
+        {
+            name: 'face',
+            img: 'images/face.png'
+        },
+        {
+            name: 'flower',
+            img: 'images/flower.png'
+        },
+        {
+            name: 'gate',
+            img: 'images/gate.png'
+        },
+        {
+            name: 'moon',
+            img: 'images/moon.png'
+        },
+        {
+            name: 'snow',
+            img: 'images/snow.png'
+        },
+
+
+    ]
+cardArray.sort(()=>0.5-Math.random())
+    const grid = document.querySelector('.grid');
+    const resultDisplay = document.querySelector('#result')
+    var cardsChosen = []
+    var cardsChosenID = []
+    var cardsWon = []
+
+function  createBoard(){
+    for (let i = 0; i<cardArray.length; i++){
+        var card = document.createElement('img')
+        card.setAttribute('src', 'images/blank.png');
+        card.setAttribute('data-id', i)
+        card.addEventListener('click',flipCard)
+        grid.appendChild(card)
+    }
+}
+   createBoard();
+    function checkForMatch(){
+        var cards = document.querySelectorAll('img')
+        const optionOneId = cardsChosenID[0]
+        const optionTwoId = cardsChosenID[1]
+        if (cardsChosen[0]===cardsChosen[1]){
+            alert('It is a match!')
+            cards[optionOneId].setAttribute('src', 'images/white.png')
+            cards[optionTwoId].setAttribute('src', 'images/white.png')
+            cardsWon.push(cardsChosen)
+        }else {
+            cards[optionOneId].setAttribute('src','images/blank.png')
+            cards[optionTwoId].setAttribute('src','images/blank.png')
+            alert('one more time')
+        }
+        cardsChosen=[]
+        cardsChosenID=[]
+        resultDisplay.textContent= cardsWon.length
+        if(cardsWon.length === cardArray.length/2){
+            resultDisplay.textContent='Win!'
+        }
+    }
+
+    function flipCard(){
+    var cardId = this.getAttribute('data-id')
+    cardsChosen.push(cardArray[cardId].name)
+    cardsChosenID.push(cardId)
+    this.setAttribute('src', cardArray[cardId].img)
+        if(cardsChosen.length===2){
+            setTimeout(checkForMatch,500)
+        }
+
+    }
+})
